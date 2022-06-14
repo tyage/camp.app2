219a8752becd758a497903e67a904968584a6bc9
tyage <namatyage@gmail.com>
2022-06-14 18:07:52 +0900

    cache static files

--- app/src/main/java/com/example/demo/HomeController.java
+++ app/src/main/java/com/example/demo/HomeController.java
@@ -1,19 +1,19 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin
 @RestController
 public class HomeController {
-  @RequestMapping("/{username}")
+  @GetMapping("/{username}")
   public String user(@PathVariable String username) {
     return username + "さんこんにちは！ お知らせは特にありません。";
   }
 
-  @RequestMapping("/admin")
+  @GetMapping("/admin")
   public String admin() {
     return "管理者向けのお知らせ: Flag{BlahBlahBlah}";
   }
