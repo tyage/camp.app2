af0a134a92fa1ca66e347687a1ece78ebaf35d57
tyage <namatyage@gmail.com>
2022-06-20 15:23:25 +0900

    move hard coded values to application.yml

--- app/src/main/java/com/example/demo/HomeController.java
+++ app/src/main/java/com/example/demo/HomeController.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +15,15 @@ public class HomeController {
   @Autowired
   HttpSession session;
 
+  @Value("${app.admin.username}")
+  private String adminUsername;
+
+  @Value("${app.admin.password}")
+  private String adminPassword;
+
+  @Value("${app.secret_message}")
+  private String secretMessage;
+
   @GetMapping("/")
   public String home() {
     return "home";
@@ -23,9 +33,10 @@ public class HomeController {
   public String login(
       @RequestParam("username") String username,
       @RequestParam("password") String password) {
-    if (username.equals("admin") &&
-        password.equals("supersecretpasswordyuruseccamp2022")) {
-      session.setAttribute("user", "admin");
+    if (username.equals(adminUsername) &&
+        password.equals(adminPassword)) {
+      // adminとしてログイン
+      session.setAttribute("user", adminUsername);
       return "redirect:/admin";
     } else {
       return "redirect:/";
@@ -35,10 +46,14 @@ public class HomeController {
   @GetMapping("/admin")
   public String admin(Model model) {
     String user = (String) session.getAttribute("user");
-    if (user == null || !user.equals("admin")) {
+
+    // admin以外のユーザはアクセス禁止
+    if (user == null || !user.equals(adminUsername)) {
       return "redirect:/";
     }
+
     model.addAttribute("user", user);
+    model.addAttribute("message", secretMessage);
     return "admin";
   }
 }
